599671e9d75b13e09663c0739e83f28cda0d4b48
木下　夏鈴 <kisitakarin@kinenatsusuzunoMacBook-Pro.local>
2024-10-22 18:36:31 +0900

    show_update

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,6 +10,6 @@ class Article < ApplicationRecord
   validates :color, presence: true
   enum category: { other: 0, stage: 1, voice_actor: 2, idol: 3}
   enum color: { red: 0, pink: 1, blue: 2, light_blue: 3, yellow: 4, green: 5, 
-                purple: 6, orange: 7, black: 8, white: 9, other_color: 10 }
+          purple: 6, orange: 7, black: 8, white: 9, other_color: 10 }
   accepts_nested_attributes_for :items ,allow_destroy: true
 end
