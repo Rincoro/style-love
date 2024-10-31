454407a02b13389ba5662b61e7ac50f68385af1e
木下　夏鈴 <kisitakarin@kinenatsusuzunoMacBook-Pro.local>
2024-10-31 23:04:41 +0900

    ogp-add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,31 @@ module ApplicationHelper
     else "bg-gray-500"
     end
   end
+
+  def default_meta_tags
+    {
+      site: 'Style-LOVE',
+      title: '推し活のコーデ投稿サービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'ライブや、推し活のコーデを共有し、自分のコーデの参考にすることができます。',
+      canonical: "https://style-love.net/"
+      separator: '|',
+      og:{
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: "https://style-love.net/"
+        image: image_url('Style-love (1).png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: image_url('Style-love (1).png')
+      }
+    }
+  end
+  
+  
 end
