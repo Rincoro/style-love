695f87eab6f9d2248d342fdadcc665267f161497
木下　夏鈴 <kisitakarin@kinenatsusuzunoMacBook-Pro.local>
2024-11-06 13:41:52 +0900

    line02

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ get "login", to: "user_sessions#new"
 post "login", to: "user_sessions#create"
 delete "logout", to: "user_sessions#destroy"
 
+
+post "oauth/callback" => "oauths#callback"
+get "oauth/callback" => "oauths#callback"
+get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
 get 'oauths/oauth', to: 'oauths#oauth', as: :oauths_oauth
-get '/oauth/callback', to: 'oauths#callback', as: 'line_callback'
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users, only: %i[new create]
   resources :items
