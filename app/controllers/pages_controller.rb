770caa2078d50f806ac01f266bf8ca709fd6529d
木下　夏鈴 <kisitakarin@kinenatsusuzunoMacBook-Pro.local>
2024-11-12 18:48:26 +0900

    top

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :require_login, only: %i[terms privacy]
+  skip_before_action :require_login, only: %i[terms privacy top]
     def terms
     end
 
     def privacy
+    end
+
+    def top
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-root "articles#index"
+root "pages#top"
 get "login", to: "user_sessions#new"
 post "login", to: "user_sessions#create"
 delete "logout", to: "user_sessions#destroy"
